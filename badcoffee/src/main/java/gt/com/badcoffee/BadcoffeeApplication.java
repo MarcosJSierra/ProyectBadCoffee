f8e44d16aefbff18cd4f87f48441797e74236c21
Marcos Sierra <marcosjspac@gmail.com>
2021-09-29 00:01:58 -0600

    agregando proyecto base

---
+++ badcoffee/src/main/java/gt/com/badcoffee/BadcoffeeApplication.java
@@ -0,0 +1,13 @@
+package gt.com.badcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadcoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadcoffeeApplication.class, args);
+	}
+
+}
